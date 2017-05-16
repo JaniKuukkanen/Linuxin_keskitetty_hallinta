e82ea1e41cb599f6d985a03d4c3261f2b1282067
JaniKuukkanen <jani.kuukkanen@myy.haaga-helia.fi>
2017-05-16 03:36:54 +0300

    Puppet code for changing the apache default website

--- init.pp
+++ init.pp
@@ -1,19 +1,20 @@
 class apache {
-				package {'apache2':
-                ensure => "installed",
+	
+	package {'apache2':
+		ensure => "installed",
 
-        }
+	}
 
-        service {'apache2':
-                enable => true,
-                ensure => "running",
+	service {'apache2':
+		enable => true,
+		ensure => "running",
 
-        }
+	}
 
-        file {'/var/www/html/index.html':
-                content => "<html><H1>Testi</H1></html>",
-                notify => Service["apache2"],
+	file {'/var/www/html/index.html':
+		content => "<html><H1>Testi</H1></html>",
+		notify => Service["apache2"],
 
-        }
+	}
 
 }
