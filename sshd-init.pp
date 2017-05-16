8b1d8b25870b4c820706d984aaaea308ecd52aae
JaniKuukkanen <jani.kuukkanen@myy.haaga-helia.fi>
2017-05-16 04:47:44 +0300

    SSHD puppet code

---
+++ sshd-init.pp
@@ -0,0 +1,21 @@
+class sshd {
+
+        package { 'openssh-server':
+                ensure => "installed",
+
+        }
+
+        service { 'ssh':
+
+                ensure => 'running',
+                enable => true,
+
+        }
+
+        file { '/etc/ssh/sshd_config':
+                content => template("/etc/puppet/modules/sshd/templates/sshd_config"),
+                notify => Service["ssh"],
+
+        }
+
+}
